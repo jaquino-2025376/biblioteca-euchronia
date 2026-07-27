1dc8b7fb6df26ac23b2c711bf1f55c3100cdc78d
jaquino-2025376 <jaquino-2025376@kinal.edu.gt>
2026-07-27 17:11:59 -0600

    Se añadio Autores al router principal

--- src/routers/router.ts
+++ src/routers/router.ts
@@ -3,6 +3,7 @@ import { usuarioRouter } from "./usuarioRouter.js";
 import { rolRouter } from "./rolRouter.js";
 import { categoriaRouter } from "./categoriaRouter.js";
 import { editorialRouter } from "./editorialRouter.js";
+import { autorRouter } from "./autorRouter.js";
 // ... importa el resto
 
 export async function router(req: IncomingMessage, res: ServerResponse) {
@@ -13,6 +14,7 @@ export async function router(req: IncomingMessage, res: ServerResponse) {
     if (await rolRouter(req, res, metodo, url)) return;
     if (await categoriaRouter(req, res, metodo, url)) return;
     if (await editorialRouter(req, res, metodo, url)) return;
+    if (await autorRouter(req, res, metodo, url)) return;
 
     res.writeHead(404, { "Content-Type": "application/json" });
     res.end(JSON.stringify({ mensaje: "Ruta no encontrada" }));
