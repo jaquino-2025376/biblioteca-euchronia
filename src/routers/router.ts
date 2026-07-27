9211fc7b20cf9ea6bfc7d48752e73a928ad8956f
jaquino-2025376 <jaquino-2025376@kinal.edu.gt>
2026-07-27 15:36:49 -0600

    Se añadio editorial al router principal

--- src/routers/router.ts
+++ src/routers/router.ts
@@ -2,6 +2,7 @@ import { IncomingMessage, ServerResponse } from "node:http";
 import { usuarioRouter } from "./usuarioRouter.js";
 import { rolRouter } from "./rolRouter.js";
 import { categoriaRouter } from "./categoriaRouter.js";
+import { editorialRouter } from "./editorialRouter.js";
 // ... importa el resto
 
 export async function router(req: IncomingMessage, res: ServerResponse) {
@@ -11,6 +12,7 @@ export async function router(req: IncomingMessage, res: ServerResponse) {
     if (await usuarioRouter(req, res, metodo, url)) return;
     if (await rolRouter(req, res, metodo, url)) return;
     if (await categoriaRouter(req, res, metodo, url)) return;
+    if (await editorialRouter(req, res, metodo, url)) return;
 
     res.writeHead(404, { "Content-Type": "application/json" });
     res.end(JSON.stringify({ mensaje: "Ruta no encontrada" }));
