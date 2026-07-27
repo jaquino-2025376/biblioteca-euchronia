4bc8ba9a392d1d70ea838be4c472b0e3a67211f7
jaquino-2025376 <jaquino-2025376@kinal.edu.gt>
2026-07-27 01:03:13 -0600

    se creo el router de categoria con sus respectivos endpoints y su ruta

---
+++ src/routers/categoriaRouter.ts
@@ -0,0 +1,41 @@
+import {IncomingMessage, ServerResponse} from "node:http";
+import {leerBody} from "../utils/leerBody.js";
+import {CategoriaController} from "../controllers/categoriaController.js";
+
+export async function categoriaRouter(
+    req: IncomingMessage,
+    res: ServerResponse,
+    metodo: string,
+    url: string
+): Promise<boolean> {
+    if (metodo === "GET" && url === "/categorias") {
+        await CategoriaController.obtenerTodos(req, res);
+        return true;
+    }
+
+    if (metodo === "POST" && url === "/categorias") {
+        const datos = await leerBody(req);
+        await CategoriaController.crear(req, res, datos);
+        return true;
+    }
+
+    const match = url.match(/^\/categorias\/(\d+)$/);
+    if (match) {
+        const id = Number(match[1]);
+        if (metodo === "GET") {
+            await CategoriaController.obtenerPorId(req, res, id);
+            return true;
+        }
+        if (metodo === "DELETE") {
+            await CategoriaController.eliminar(req, res, id);
+            return true;
+        }
+        if (metodo === "PUT") {
+            const datos = await leerBody(req);
+            await CategoriaController.actualizar(req, res, id, datos);
+            return true;
+        }
+    }
+
+    return false; // no coincidió ninguna ruta de este módulo
+}
