efed5eef5c94561ad068fbf224ca5e9f423d1f7a
jaquino-2025376 <jaquino-2025376@kinal.edu.gt>
2026-07-28 01:38:16 -0600

    se añadieron todos los routers al original

--- src/routers/router.ts
+++ src/routers/router.ts
@@ -5,6 +5,11 @@ import { categoriaRouter } from "./categoriaRouter.js";
 import { editorialRouter } from "./editorialRouter.js";
 import { autorRouter } from "./autorRouter.js";
 import { libroRouter } from "./libroRouter.js";
+import { ejemplarRouter } from "./ejemplarRouter.js";
+import { prestamoRouter } from "./prestamoRouter.js";
+import { multaRouter } from "./multaRouter.js";
+import { reservaRouter } from "./reservaRouter.js";
+import { reseniaRouter } from "./reseniaRouter.js";
 
 export async function router(req: IncomingMessage, res: ServerResponse) {
     const metodo = req.method ?? "";
@@ -16,6 +21,11 @@ export async function router(req: IncomingMessage, res: ServerResponse) {
     if (await editorialRouter(req, res, metodo, url)) return;
     if (await autorRouter(req, res, metodo, url)) return;
     if (await libroRouter(req, res, metodo, url)) return;
+    if (await ejemplarRouter(req, res, metodo, url)) return;
+    if (await prestamoRouter(req, res, metodo, url)) return;
+    if (await multaRouter(req, res, metodo, url)) return;
+    if (await reservaRouter(req, res, metodo, url)) return;
+    if (await reseniaRouter(req, res, metodo, url)) return;
 
     res.writeHead(404, { "Content-Type": "application/json" });
     res.end(JSON.stringify({ mensaje: "Ruta no encontrada" }));
